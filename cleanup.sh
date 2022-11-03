48030b4719125d4f82c3a23c3f91dcf5a8def189
Martin Emrich <martin.emrich@empolis.com>
2022-11-03 15:27:31 +0100

    also clean up Terraform local files

--- cleanup.sh
+++ cleanup.sh
@@ -7,5 +7,7 @@ fi
 export AWS_REGION=eu-central-1
 export AWS_DEFAULT_REGION=eu-central-1
 
-aws ec2 delete-key-pair --key-name tf-import-test && rm -f tf-import-test.pm
 
+aws ec2 delete-key-pair --key-name tf-import-test && rm -f tf-import-test.pm
+rm -rf tf/.terraform
+rm -f tf/.terraform.lock.hcl
